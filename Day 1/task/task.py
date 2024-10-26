cf7e19a4f257838902fdaf2a01eb5bb080c48c93
Medina <MedinaCa@hnicorp.com>
2024-10-26 13:30:07 -0600

    Day 1 completed

---
+++ Day 1/task/task.py
@@ -0,0 +1,4 @@
+print("Greetings! this is a band name generator project 7000 ultra awesome!")
+city_name = input("What's the name of the place where you grow up?\n")
+pet_name = input("What's the name of your pet?\n")
+print("Your band should be named: " + pet_name + " " + city_name)
