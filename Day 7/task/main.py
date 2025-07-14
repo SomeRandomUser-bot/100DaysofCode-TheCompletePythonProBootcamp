e7c0f20c3550d948623673eab06850bb417a225a
Carlos <carlos_medina_245@outlook.es>
2025-07-14 15:07:23 -0600

    files

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -1,8 +1,54 @@
-word_list = ["aardvark", "baboon", "camel"]
+import random
+import hangman_words
+import hangman_art
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
 
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+lives = 6
 
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
-#  is, "Wrong" if it's not.
+chosen_word = random.choice(hangman_words.word_list)
+print(hangman_art.logo)
+print(chosen_word)
+
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
+print("Word to guess: " + placeholder)
+print(hangman_art.stages[lives])
+
+game_over = False
+correct_letters = []
+
+while not game_over:
+
+    print("**************************** " + str(lives) + "/6 LIVES LEFT****************************")
+    guess = input("Guess a letter: ").lower()
+
+    display = ""
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+            print("Letter already guessed, try again.")
+        else:
+            display += "_"
+
+    print("Word to guess: " + display)
+
+    if guess not in chosen_word:
+        lives -= 1
+        print("that's not in the word. You lose a life.")
+        if lives == 0:
+            game_over = True
+
+            print(f"***********************YOU LOSE**********************")
+            print("The word was " + chosen_word)
+
+    if "_" not in display:
+        game_over = True
+        print("****************************YOU WIN****************************")
+
+    print(hangman_art.stages[lives])
