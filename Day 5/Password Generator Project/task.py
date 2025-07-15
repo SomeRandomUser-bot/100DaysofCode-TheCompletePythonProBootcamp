4af5f9a84bcceeb9c98fed95eb45b806f4000181
Carlos <carlos_medina_245@outlook.es>
2025-07-15 16:21:35 -0600

    files

--- Day 5/Password Generator Project/task.py
+++ Day 5/Password Generator Project/task.py
@@ -1,10 +1,19 @@
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+import string
+import random
 
 print("Welcome to the PyPassword Generator!")
 nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+dictionary=string.digits+string.ascii_letters+string.punctuation
+password_total_length = nr_letters + nr_symbols + nr_numbers
 
+pass1="".join(random.choice(string.digits)for _ in range(nr_numbers))
+pass2="".join(random.choice(string.ascii_letters)for _ in range(nr_letters))
+pass3="".join(random.choice(string.punctuation)for _ in range(nr_symbols))
+
+pass_gen = list(pass1 + pass2 + pass3)
+
+random.shuffle(pass_gen)
+print("Your generated password is: "+ "".join(pass_gen))
--- Day 8/Caesar Cipher 3/main.py
+++ Day 8/Caesar Cipher 3/main.py
@@ -1,32 +1,76 @@
-# TODO-1: Import and print the logo from art.py when the program starts.
-
+# # TODO-1: Import and print the logo from art.py when the program starts.
+# import art
+#
+# alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+#
+# # TODO-2: What happens if the user enters a number/symbol/space?
+#
+#
+# def caesar(original_text, shift_amount, encode_or_decode):
+#     output_text = ""
+#
+#     for letter in original_text:
+#         if encode_or_decode == "decode":
+#             shift_amount *= -1
+#
+#         shifted_position = alphabet.index(letter) + shift_amount
+#         shifted_position %= len(alphabet)
+#         output_text += alphabet[shifted_position]
+#     print(f"Here is the {encode_or_decode}d result: {output_text}")
+#
+#
+# # TODO-3: Can you figure out a way to restart the cipher program?
+#
+# print(art.logo)
+# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+# text = input("Type your message:\n").lower()
+# shift = int(input("Type the shift number:\n"))
+#
+#
+# caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+', ' ']
 
-# TODO-2: What happens if the user enters a number/symbol/space?
+alphabet.extend(numbers)
+alphabet.extend(symbols)
+finished = False
+restart = ""
 
+import art
 
-def caesar(original_text, shift_amount, encode_or_decode):
-    output_text = ""
+def caesar(original_text, shift_amount, encode_decode_choice):
+    cyphertext = ""
+    if encode_decode_choice == "encode":
+        for letter in original_text:
+            shiftletter = alphabet.index(letter) + shift_amount
+            shiftletter %= len(alphabet) # Using Modulo will grants us to always stay in 0-25 range
+            cyphertext += alphabet[shiftletter]
+        print(cyphertext)
+    else:
+        for letter in original_text:
+            shiftletter = alphabet.index(letter) - shift_amount
+            shiftletter %= len(alphabet) # Using Modulo will grants us to always stay in 0-25 range
+            cyphertext += alphabet[shiftletter]
+        print(cyphertext)
 
-    for letter in original_text:
-        if encode_or_decode == "decode":
-            shift_amount *= -1
-
-        shifted_position = alphabet.index(letter) + shift_amount
-        shifted_position %= len(alphabet)
-        output_text += alphabet[shifted_position]
-    print(f"Here is the {encode_or_decode}d result: {output_text}")
-
-
-# TODO-3: Can you figure out a way to restart the cipher program?
-
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
-
-
-
+print(art.logo)
+while not finished:
+    restart = ""
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    if direction == "encode" or direction == "decode":
+        text = input("Type your message:\n").lower()
+        shift = int(input("Type the shift number:\n"))
+        caesar(text, shift, direction)
+    else:
+        print("Wrong answerer, please try again")
+    while restart != "yes" and restart != "no":
+        restart = input("Do you want to continue? Yes/No\n").lower()
+        if restart == "yes":
+            finished = False
+        elif restart == "no":
+            finished = True
+            print("Goodbye!")
+        else:
+            print("Wrong answer, please try again.")
